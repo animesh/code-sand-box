8d889a2c34298ce29116ad507dae5f7535f24a2c
ASKHPC <sharma.animesh@gmail.com>
2013-03-27 15:34:43 +0100

    simulate test

--- assess-folio.py
+++ assess-folio.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def simulate(dt_start, dt_end,ls_symbols ,salloc ):
+def simulate(dt_start, dt_end,ls_symbols ,ls_alloc ):
     dt_timeofday = dt.timedelta(hours=16)
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+    #c_dataobj = da.DataAccess('Yahoo', cachestalltime=0)
     c_dataobj = da.DataAccess('Yahoo')
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
@@ -16,30 +17,41 @@ def simulate(dt_start, dt_end,ls_symbols ,salloc ):
     na_price = d_data['close'].values
     na_normalized_price = na_price / na_price[0, :]
     na_rets = na_normalized_price.copy()
-    #print tsu.returnize0(na_rets)
-    #plt.clf()
-    #plt.plot(ldt_timestamps, na_normalized_price)
-    #plt.legend(ls_symbols)
-    #plt.ylabel('Adjusted Close')
-    #plt.xlabel('Date')
-    #plt.show()
-    #plt.scatter(na_rets[:, 3], na_rets[:, 1], c='blue')
+    tsu.returnize0(na_rets)
     dret=d_data['open'].values-d_data['close'].values
     dvol=d_data['volume'].values
     cum_ret = np.mean(d_data['close'].values * (1 + d_data['close'].values))
+    portfolio_daily_rets = np.dot(na_rets,ls_alloc) 
     k_sharpe=np.sqrt(252)
-    dr_mean=np.mean(d_data['close'].values/d_data['open'].values-1)
-    dr_sd=np.std(d_data['close'].values/d_data['open'].values-1)
-    sharpe_ratio = k_sharpe*dr_mean/dr_sd
-    #portfolio_daily_rets = 0.75 * GLD_daily_rets + 0.25 * SPY_daily_rets
-    return dvol, dret, sharpe_ratio, cum_ret 
-    
-slist=['AAPL','GLD','GOOG','XOM']
-salloc=[0.4, 0.4, 0.0, 0.2]
-sd=dt.datetime(2011, 1, 1)
-se=dt.datetime(2011, 12, 31)
-vol, daily_ret, sharpe, cum_ret = simulate(sd,se,slist ,salloc)
-print sharpe, np.mean(vol), cum_ret, daily_ret
+    pdr_mu=np.mean(portfolio_daily_rets)
+    pdr_sig=np.std(portfolio_daily_rets)
+    sharpe_ratio = k_sharpe*pdr_mu/pdr_sig
+    na_port_total = np.cumprod(portfolio_daily_rets + 1)
+    na_component_total = np.cumprod(portfolio_daily_rets + 1, axis=0)
+    cum_ret = np.cumprod(na_port_total + 1, axis=0)
+    return sharpe_ratio, pdr_sig, pdr_mu, na_port_total[-1]
+
+maxsr=float("-inf")
+maxalloc=""
+n=10
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            slist= ['AAPL','GLD','GOOG','XOM']
+            if 1-((i+j+k)/float(n))>=0:
+                print i/float(n), j/float(n), k/float(n), 1-((i+j+k)/float(n))
+                salloc=[round(i/float(n),2), round(j/float(n),2), round(k/float(n),2), round(1-((i+j+k)/float(n)),2)]
+                sd=dt.datetime(2011, 1, 1)
+                se=dt.datetime(2011, 12, 31)
+                sr,sig,mu,cr=simulate(sd,se,slist ,salloc)
+                if sr>maxsr:
+                    maxsr=sr
+                    maxalloc=salloc
+print i,j,k,slist,salloc,sd,se, sr,sig,mu,cr, maxalloc,maxsr
+
+                
+
 
 '''
 http://wiki.quantsoftware.org/index.php?title=QSTK_License
@@ -51,4 +63,8 @@ https://code.google.com/hosting/settings
 https://class.coursera.org/compinvesting1-002/quiz/start?quiz_id=188
 https://piazza.com/class#spring2013/1/606
 https://www.coursera.org/user/i/185bb8b16b9f8734c33cb05b6484de4b
+slist= ['AXP', 'HPQ', 'IBM', 'HNZ'] #['AAPL','GLD','GOOG','XOM']
+salloc=[0.0, 0.0, 0.0, 1.0]#[0.4, 0.4, 0.0, 0.2]
+sd=dt.datetime(2010, 1, 1)
+se=dt.datetime(2010, 12, 31)
 '''
--- nseega.py
+++ nseega.py
@@ -8,7 +8,10 @@ spectrum, relative_spectrum = bin_power(a, [0.5,4,7,12,30],512)
 print spectrum, relative_spectrum
 
 
+
 '''
-http://pyeeg.org
-https://github.com/akloster/python-mindwave
+http://code.google.com/p/pyeeg/source/browse/pyeeg.py
+https://github.com/akloster/python-mindwave/blob/master/feedback.py
+https://code.google.com/p/code-sand-box/source/browse/nseega.py
+https://code.google.com/hosting/settings
 '''
