7541baa91f52e0135c6c76e11b518025d9a70c8d
ASKHPC <sharma.animesh@gmail.com>
2013-03-28 17:32:38 +0100

    reading and converting to int

--- nseega.py
+++ nseega.py
@@ -1,10 +1,11 @@
 from pyeeg import *
 from numpy.random import randn
 
-a = randn(4096)
-print a
-#print hurst(a)
-spectrum, relative_spectrum = bin_power(a, [0.5,4,7,12,30],512)
+with open("eegsigfrg.txt") as input:
+    sig = zip(*(line.strip().split('\t') for line in input))
+ls = [map(int, x) for x in sig]
+print hurst(ls[:][1])
+spectrum, relative_spectrum = bin_power((zip(*ls)[1]), [0.5,4,7,12,30],512)
 print spectrum, relative_spectrum
 
 
@@ -15,4 +16,6 @@ https://github.com/akloster/python-mindwave/blob/master/feedback.py
 https://code.google.com/p/code-sand-box/source/browse/nseega.py
 https://code.google.com/p/misccb/source/browse/geteegfromns.m
 https://code.google.com/hosting/settings
+http://stackoverflow.com/questions/11059390/parsing-a-tab-separated-file-in-python
+http://stackoverflow.com/questions/1909619/python-list-to-integers/1909632#1909632
 '''
