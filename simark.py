088d58d38d9361ad3926822a1aba3030e4f08dae
U-WIN-NTNU-NO\animeshs <animeshs@DMED5714.klientmaskiner.medisin.ntnu.no>
2013-04-07 00:27:56 +0200

    convert and write folio from market simulator

--- simark.py
+++ simark.py
@@ -5,23 +5,27 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 
+amt=1000000
+inp='orders2.csv'
+otp='values.csv'
+f = open(otp, 'w')
 
-data = np.loadtxt('orders.csv', delimiter=',',
+data = np.loadtxt(inp, delimiter=',',
             dtype={'names': ('year', 'month','day','ticker','action','number'),
-                'formats': ('f2','f2','f2','S5','S5','I2')})
+                'formats': ('I2','I1','I1','S6','S4','I2')})
 sym = data['ticker']
 vol = data['number']  
 day = []
-for i in range(0, len(data)):
-    day.append(dt.date(data['year'].astype(int)[i],data['month'].astype(int)[i],
-                                  data['day'].astype(int)[i]))
-
+for i in range(len(data)):
+    day.append(dt.date(data['year'][i],data['month'][i],
+                                  data['day'][i]))
+    f.write("%s,%d\n" % (day[i],amt))
+    
 print sym,vol,day
 
+f.close()
 
 '''
 http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_3
 http://wiki.quantsoftware.org/index.php?title=QSTK_Tutorial_2
-http://stackoverflow.com/questions/13752988/usecols-in-loadtxt
-http://stackoverflow.com/questions/10873824/how-to-convert-2d-float-numpy-array-to-2d-int-numpy-array
 '''
