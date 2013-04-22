6c8835b85c9309dc0082622c8342e955e62e5470
ASKHPC <sharma.animesh@gmail.com>
2013-04-22 18:19:14 +0200

    event on bollinger bands

--- bollband-event.py
+++ bollband-event.py
@@ -2,10 +2,13 @@ import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkutil.DataAccess as da
 import QSTK.qstksim as qstksim
+import QSTK.qstkstudy.EventProfiler as ep
 import datetime as dt
 import pandas as pa
 import numpy as np
 #from matplotlib.pyplot import *
+import math
+import copy
 
 def find_events(ls_symbols, d_data):
 
@@ -51,26 +54,29 @@ if __name__ == '__main__':
                 s_market_sym='SPY')
 
 
-sym = ['GOOG']
-sym = ['AAPL']
-sym = ['IBM']
-sym = ['MSFT']
-sym = ['SPY']
+    sym = ['GOOG']
+    sym = ['AAPL']
+    sym = ['IBM']
+    sym = ['MSFT']
+    sym = ['SPY']
 
-sd = dt.datetime(2010,1,1)
-ed = dt.datetime(2010,12,31)
-pd = 20
-inc = 1
-ts = du.getNYSEdays(sd,ed,dt.timedelta(hours=16))
-dobj = da.DataAccess('Yahoo')
-cp = dobj.get_data(ts, sym, "close",verbose=True)
+    sd = dt.datetime(2010,1,1)
+    ed = dt.datetime(2010,12,31)
+    pd = 20
+    inc = 1
+    ts = du.getNYSEdays(sd,ed,dt.timedelta(hours=16))
+    dobj = da.DataAccess('Yahoo')
+    cp = dobj.get_data(ts, sym, "close",verbose=True)
 
-for i in range(pd-inc,len(cp)):
-    bollval = (cp.values[i] - np.mean(cp.values[i-pd+inc:i+inc])) / (np.std(cp.values[i-pd+inc:i+inc]))
-    print i,ts[i],cp.values[i],bollval #,cp.values[i-pd+inc:i+inc]
+    for i in range(pd-inc,len(cp)):
+        bollval = (cp.values[i] - np.mean(cp.values[i-pd+inc:i+inc])) / (np.std(cp.values[i-pd+inc:i+inc]))
+        print i,ts[i],cp.values[i],bollval #,cp.values[i-pd+inc:i+inc]
 
 
     
 '''
+http://cran.r-project.org/doc/contrib/Fox-Companion/appendix-mixed-models.pdf
 http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_5
+http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_6
+http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_7
 '''
